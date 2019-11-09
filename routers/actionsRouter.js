ca758d6d0e7c81a65cc653b245d7bef42d7529ea
kly001 <karenly1961@gmail.com>
2019-11-09 13:46:24 -0500

    projectsRouter still needs work

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
         res.status(500).json({message:"Sorry, could not retrieve actions information."})
     })
 });
-//----------------------------------------------------------------------------------------
+//------------------------------------------------------------------
 
 router.get('/:id',(req, res) => {
     const {id} = req.params
@@ -22,7 +22,7 @@ router.get('/:id',(req, res) => {
         res.status(500).json({ errorMessage: "Error getting action."})
     })
 });
-//---------------------------------------------------------------------------------------
+//----------------------------------------------------------------
 
 router.post("/",(req,res) => {
     const actionInfo = req.body
@@ -37,6 +37,33 @@ router.post("/",(req,res) => {
 
 //----------------------------------------------------------------
 
+router.delete("/:id",(req,res) => {
+    const {id} = req.params
+    Actions.remove(id) 
+    .then(delAction => {
+        if(delAction>0) {
+            res.status(200).json({message:"The action has been deleted."})
+        } else {
+            res.status(404).json({message:`The action with ID# ${id} does not exist.`})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message:"The action could not be removed."})
+    })
+})
+//----------------------------------------------------------------
+
+router.put("/:id",(req, res) => {
+    const {id} = req.params
+    const changes = req.body
+   Actions.update(id,changes)
+    .then(action => {
+        res.status(201).json(action)
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage:"Unable to update action."})
+    })
+});
 
 
 module.exports = router
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const Projects = require("../data/helpers/projectModel.js")
-const Actions = require("../data/helpers/actionModel.js")
+
 
 router.get("/", (req, res) => {
     Projects.get()
@@ -11,6 +11,59 @@ router.get("/", (req, res) => {
         res.status(500).json({message:"Sorry, could not retrieve projects information."})
     })
 });
+//------------------------------------------------------
 
+router.get('/:id',(req, res) => {
+    const {id} = req.params
+    Projects.get(id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Error getting project."})
+    })
+});
+//----------------------------------------------------------------
+
+router.post("/",(req,res) => {
+    const projectInfo = req.body
+        Projects.insert(projectInfo) 
+        .then(project=> {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({error: "There was an error while saving the project to the database."})
+    }) 
+})
+
+//----------------------------------------------------------------
+
+router.delete("/:id",(req,res) => {
+    const {id} = req.params
+    Projects.remove(id) 
+    .then(delProject => {
+        if(delProject>0) {
+            res.status(200).json({message:"The project has been deleted."})
+        } else {
+            res.status(404).json({message:`The project with ID# ${id} does not exist.`})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message:"The project could not be removed."})
+    })
+})
+//----------------------------------------------------------------
+
+router.put("/:id",(req, res) => {
+    const {id} = req.params
+    const changes = req.body
+   Projects.update(id,changes)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage:"Unable to update project."})
+    })
+});
 
 module.exports = router
